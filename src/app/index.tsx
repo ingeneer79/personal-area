949d51f5a9775bc8ef2170d4cb05c1add824311a
Сергей Яшин <sergey.yashin@nvi-solutions.com>
2024-06-18 19:51:36 +0300

    Redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,19 +3,22 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@/app/styles/index.css";
 import React, { PropsWithChildren } from "react";
 import { ConfigLayout } from '../widgets/layouts/config-layout/ui';
+import { StoreProvider } from './providers/store-provider';
 
 const inter = Inter({ subsets: ["cyrillic", "latin", "cyrillic-ext"] });
 
 export const AppRoot = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <AntdRegistry>
-          <ConfigLayout>
-            {children}
-          </ConfigLayout>
-      </AntdRegistry>                  
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className={inter.className}>
+        <AntdRegistry>
+            <ConfigLayout>
+              {children}
+            </ConfigLayout>
+        </AntdRegistry>                  
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
