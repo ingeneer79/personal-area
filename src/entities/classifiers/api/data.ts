a11ce1be01168c73c138e41098c3e980a7e885d4
Сергей Яшин <sergey.yashin@nvi-solutions.com>
2024-07-11 00:52:05 +0300

    Deploy

--- src/entities/classifiers/api/data.ts
+++ src/entities/classifiers/api/data.ts
@@ -2,9 +2,11 @@ import { apiMap, constantsMap } from "@/shared/model"
 import { ClassifierObject } from "../model"
 
 export async function getClassifiers(): Promise<ClassifierObject[]> {
-    const res = await fetch(`${constantsMap.shared.config.apiUrl}/${apiMap.getClassifiers}`)   
+  // const res = await fetch(`${constantsMap.shared.config.apiUrl}/${apiMap.getClassifiers}`)     
+  const res = await fetch(`${constantsMap.shared.config.apiUrl}`)   
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }   
-    return res.json()
+    const classifiers = await res.json();
+    return Promise.resolve(classifiers.classifiers)
   }
