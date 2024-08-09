58ca467e50b1d74abfef54a0108db454e2a73aed
Vyacheslav <vyacheslav.kruglikov@nvi-solutions.com>
2024-08-09 15:35:27 +0300

    petitions modal WIP

--- src/entities/petitions/model/slice.ts
+++ src/entities/petitions/model/slice.ts
@@ -2,10 +2,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface PetitionsState {
   newPetition: boolean
+  newPetitionModalOpen: boolean
+
 }
 
 const initialState: PetitionsState = {
-    newPetition: false
+    newPetition: false,
+    newPetitionModalOpen: false
 };
 
 export const petitionsSlice = createSlice({
@@ -14,6 +17,9 @@ export const petitionsSlice = createSlice({
   reducers: {
     addNewOrder: (state, action: PayloadAction<boolean>) => {
         state.newPetition = action.payload
+    },
+    setNewPetitionModalOpen: (state, action: PayloadAction<boolean>) => {
+        state.newPetitionModalOpen = !action.payload
     }
   },
 });
